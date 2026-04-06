318e5a74ceb4ba4cad342181102d823acd22d171
Guy Mozes <69205951+GuyMozes7026@users.noreply.github.com>
2026-04-06 19:55:16 +0300

    chore: rename database.js to catabase.ts git doesn't accept it as a rename with the chagnes

--- src/database.ts
+++ src/database.ts
@@ -45,7 +45,7 @@ async function connect() {
 	let hasNetworkModApps = false;
 	let hasJuxtModApps = false;
 	await database.each('SELECT * FROM pragma_table_info(\'server_settings\')', (_err, row) => {
-		switch(row.name) {
+		switch (row.name) {
 			case 'ay_lmao_disabled':
 				hasAyLmaoColumn = true;
 				break;
@@ -116,53 +116,53 @@ async function connect() {
 }
 
 async function initGuild(guildId) {
-	await database.run('INSERT OR IGNORE INTO server_settings(guild_id) VALUES(?)', [ guildId ]);
+	await database.run('INSERT OR IGNORE INTO server_settings(guild_id) VALUES(?)', [guildId]);
 }
 
 async function getGuildSetting(guildId, name) {
-	return (await database.get(`SELECT ${name} FROM server_settings WHERE guild_id=?`, [ guildId ]))[name];
+	return (await database.get(`SELECT ${name} FROM server_settings WHERE guild_id=?`, [guildId]))[name];
 }
 
 async function updateGuildSetting(guildId, name, value) {
-	await database.run(`UPDATE server_settings SET ${name}=? WHERE guild_id=?`, [ value, guildId ]);
+	await database.run(`UPDATE server_settings SET ${name}=? WHERE guild_id=?`, [value, guildId]);
 }
 
 async function checkAutomaticHelpDisabled(guildId, memberId) {
-	const result = await database.get('SELECT EXISTS (SELECT 1 FROM nlp_disabled WHERE guild_id=? AND member_id=? LIMIT 1)', [ guildId, memberId ]);
+	const result = await database.get('SELECT EXISTS (SELECT 1 FROM nlp_disabled WHERE guild_id=? AND member_id=? LIMIT 1)', [guildId, memberId]);
 	return Object.values(result)[0]; // * Hack. sqlite returns objects not values, need to get the value from the object
 }
 
 async function disableAutomaticHelp(guildId, memberId) {
-	await database.run('INSERT OR IGNORE INTO nlp_disabled(guild_id, member_id) VALUES(?, ?)', [ guildId, memberId ]);
+	await database.run('INSERT OR IGNORE INTO nlp_disabled(guild_id, member_id) VALUES(?, ?)', [guildId, memberId]);
 }
 
 async function enabledAutomaticHelp(guildId, memberId) {
-	await database.run('DELETE FROM nlp_disabled WHERE guild_id=? AND member_id=?', [ guildId, memberId ]);
+	await database.run('DELETE FROM nlp_disabled WHERE guild_id=? AND member_id=?', [guildId, memberId]);
 }
 
 async function checkAyLmaoDisabled(guildId) {
-	const result = await database.get('SELECT ay_lmao_disabled FROM server_settings WHERE guild_id=? LIMIT 1', [ guildId ]);
+	const result = await database.get('SELECT ay_lmao_disabled FROM server_settings WHERE guild_id=? LIMIT 1', [guildId]);
 	return Boolean(result.ay_lmao_disabled);
 }
 
 async function initMemberCooldown(memberId, commandId) {
-	await database.run('INSERT OR IGNORE INTO command_cooldowns(member_id, command_id, cooldown) VALUES(?, ?, ?)', [ memberId, commandId, 0]);
+	await database.run('INSERT OR IGNORE INTO command_cooldowns(member_id, command_id, cooldown) VALUES(?, ?, ?)', [memberId, commandId, 0]);
 }
 
 async function updateCommandCooldown(memberId, commandId, cooldown) {
-	await database.run('UPDATE command_cooldowns SET cooldown=? WHERE member_id=? AND command_id=?', [ cooldown, memberId, commandId ]);
+	await database.run('UPDATE command_cooldowns SET cooldown=? WHERE member_id=? AND command_id=?', [cooldown, memberId, commandId]);
 }
 
 async function getCommandCooldown(memberId, commandId) {
-	return (await database.get('SELECT cooldown FROM command_cooldowns WHERE member_id=? AND command_id=?', [ memberId, commandId ]))['cooldown'];
+	return (await database.get('SELECT cooldown FROM command_cooldowns WHERE member_id=? AND command_id=?', [memberId, commandId]))['cooldown'];
 }
 
 async function createPoll(guildId, pollId, channelId, title, expiryTime, options) {
-	await database.run('INSERT OR IGNORE INTO polls(guild_id, poll_id, channel_id, title, expiry_time, options) VALUES(?, ?, ?, ?, ?, ?)',  [ guildId, pollId, channelId, title, Number(expiryTime.toString().padEnd(13, '0')), JSON.stringify(options)]);
+	await database.run('INSERT OR IGNORE INTO polls(guild_id, poll_id, channel_id, title, expiry_time, options) VALUES(?, ?, ?, ?, ?, ?)', [guildId, pollId, channelId, title, Number(expiryTime.toString().padEnd(13, '0')), JSON.stringify(options)]);
 }
 
 async function votePoll(memberId, pollId, vote) {
-	const result = await database.get('SELECT votes, voters FROM polls WHERE poll_id=?', [ pollId ]);
+	const result = await database.get('SELECT votes, voters FROM polls WHERE poll_id=?', [pollId]);
 	const votes = JSON.parse(Object.values(result)[0]);
 	const voters = JSON.parse(Object.values(result)[1]);
 
@@ -170,7 +170,7 @@ async function votePoll(memberId, pollId, vote) {
 		votes[vote] += 1;
 		voters.push(memberId);
 
-		await database.run('UPDATE polls SET votes = ?, voters = ? WHERE poll_id=?', [ JSON.stringify(votes), JSON.stringify(voters), pollId ]);
+		await database.run('UPDATE polls SET votes = ?, voters = ? WHERE poll_id=?', [JSON.stringify(votes), JSON.stringify(voters), pollId]);
 
 		return true;
 	} else {
@@ -179,7 +179,7 @@ async function votePoll(memberId, pollId, vote) {
 }
 
 async function getPollInfo(pollId) {
-	const result = await database.get('SELECT title, options, votes, expiry_time FROM polls WHERE poll_id=?', [ pollId ]);
+	const result = await database.get('SELECT title, options, votes, expiry_time FROM polls WHERE poll_id=?', [pollId]);
 
 	const title = Object.values(result)[0];
 	const options = JSON.parse(Object.values(result)[1]);
@@ -207,11 +207,11 @@ async function getAllPollInfo() {
 }
 
 async function closePoll(pollId) {
-	await database.run('DELETE FROM polls WHERE poll_id=?', [ pollId ]);
+	await database.run('DELETE FROM polls WHERE poll_id=?', [pollId]);
 }
 
 async function doesPollExist(pollId) {
-	const poll = await database.get('SELECT COUNT(*) FROM polls WHERE poll_id=? LIMIT 1', [ pollId ]);
+	const poll = await database.get('SELECT COUNT(*) FROM polls WHERE poll_id=? LIMIT 1', [pollId]);
 	if (Object.values(poll)[0] === 0) {
 		return false;
 	} else {
@@ -220,23 +220,23 @@ async function doesPollExist(pollId) {
 }
 
 async function createRule(guildId, title, description, time) {
-	await database.run('INSERT INTO rules(guild_id, title, description, time) VALUES(?, ?, ?, ?)', [ guildId, title, description, time ]);
+	await database.run('INSERT INTO rules(guild_id, title, description, time) VALUES(?, ?, ?, ?)', [guildId, title, description, time]);
 }
 
 async function updateRule(guildId, id, title, description, time) {
-	await database.run('INSERT OR REPLACE INTO rules(guild_id, id, title, description, time) VALUES(?, ?, ?, ?, ?)', [ guildId, id, title, description, time ]);
+	await database.run('INSERT OR REPLACE INTO rules(guild_id, id, title, description, time) VALUES(?, ?, ?, ?, ?)', [guildId, id, title, description, time]);
 }
 
 async function getRule(guildId, ruleId) {
-	return (await database.get('SELECT id, title, description, time FROM rules WHERE guild_id=? AND id=?', [ guildId, ruleId ]));
+	return (await database.get('SELECT id, title, description, time FROM rules WHERE guild_id=? AND id=?', [guildId, ruleId]));
 }
 
 async function getAllRules(guildId) {
-	return (await database.all('SELECT id, title, description, time FROM rules WHERE guild_id=?', [ guildId ]));
+	return (await database.all('SELECT id, title, description, time FROM rules WHERE guild_id=?', [guildId]));
 }
 
 async function removeRule(guildId, ruleId) {
-	await database.run('DELETE FROM rules WHERE guild_id=? AND id=?', [ guildId, ruleId ]);
+	await database.run('DELETE FROM rules WHERE guild_id=? AND id=?', [guildId, ruleId]);
 }
 
 module.exports = {
