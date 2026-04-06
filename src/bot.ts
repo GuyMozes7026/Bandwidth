6fea83e738204fdadf7c1635adff1aca7d41a434
Guy Mozes <69205951+GuyMozes7026@users.noreply.github.com>
2026-04-06 20:02:13 +0300

    chore(bot.ts): import only what's needed and add types to Collections

--- src/bot.ts
+++ src/bot.ts
@@ -1,4 +1,4 @@
-import Discord from 'discord.js';
+import { Client, GatewayIntentBits, Partials, Collection, Events } from 'discord.js';
 import AIMessageProcessor from '@/nlp/message-processor';
 import readyHandler from '@/events/ready';
 import guildMemberAddHandler from '@/events/guildMemberAdd';
@@ -6,33 +6,34 @@ import interactionCreateHandler from '@/events/interactionCreate';
 import messageCreateHandler from '@/events/messageCreate';
 import threadCreateHandler from '@/events/threadCreate';
 import config from '@/../config.json';
+import type { ButtonHandler, CommandHandler, ContextMenuHandler, ModalHandler, SelectMenuHandler } from './types/general-types';
 
-const client = new Discord.Client({
+const client = new Client({
 	intents: [
-		Discord.GatewayIntentBits.DirectMessages,
-		Discord.GatewayIntentBits.Guilds,
-		Discord.GatewayIntentBits.GuildMessages,
-		Discord.GatewayIntentBits.GuildMembers,
-		Discord.GatewayIntentBits.MessageContent
+		GatewayIntentBits.DirectMessages,
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.GuildMembers,
+		GatewayIntentBits.MessageContent
 	],
 	partials: [
-		Discord.Partials.Channel,
-		Discord.Partials.Message
+		Partials.Channel,
+		Partials.Message
 	]
 });
 
 client.aiMessageProcessor = new AIMessageProcessor();
 
-client.buttons = new Discord.Collection();
-client.commands = new Discord.Collection();
-client.contextMenus = new Discord.Collection();
-client.modals = new Discord.Collection();
-client.selectMenus = new Discord.Collection();
+client.buttons = new Collection<string, ButtonHandler>();
+client.commands = new Collection<string, CommandHandler>();
+client.contextMenus = new Collection<string, ContextMenuHandler>();
+client.modals = new Collection<string, ModalHandler>();
+client.selectMenus = new Collection<string, SelectMenuHandler>();
 
-client.on(Discord.Events.ClientReady, readyHandler);
-client.on(Discord.Events.GuildMemberAdd, guildMemberAddHandler);
-client.on(Discord.Events.InteractionCreate, interactionCreateHandler);
-client.on(Discord.Events.MessageCreate, messageCreateHandler);
-client.on(Discord.Events.ThreadCreate, threadCreateHandler);
+client.on(Events.ClientReady, readyHandler);
+client.on(Events.GuildMemberAdd, guildMemberAddHandler);
+client.on(Events.InteractionCreate, interactionCreateHandler);
+client.on(Events.MessageCreate, messageCreateHandler);
+client.on(Events.ThreadCreate, threadCreateHandler);
 
 client.login(config.bot_token);
